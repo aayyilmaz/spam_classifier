d98b44c676c64ca92d6ba8a09d5ad44b6cca7bca
aayyilmaz <aayyilmaz@gmail.com>
2019-03-03 14:37:49 +0300

    color prpgram added

--- color.py
+++ color.py
@@ -4,3 +4,6 @@ i=0
 while i < len(color):
     print ("when i was %d, my favorite color was %s" % (i,color[i]))
     i=i+1
+git remote set-url origin https://aayyilmaz@github.com/user/repo.git
+
+git remote set-url origin https://aayyilmaz@github.com/user/repo.git
